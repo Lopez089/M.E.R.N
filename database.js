9414b15a32e2bfd7e346da8a4f7b2bdcb1e13f1e
Juan Lopez <contacto@juanlopezaragon.es>
2019-08-19 11:54:28 +0200

    inicial

---
+++ database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const URI = 'mongodb://localhost/task';
+
+mongoose.connect(URI,  {useNewUrlParser: true})
+    .then(db => console.log('BD is connectd'))
+    .catch(err => console.log(err))
+
+module.exports = mongoose;
